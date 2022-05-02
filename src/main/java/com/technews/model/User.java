0351363c7044bd51a4436f142aa03fda49ed0455
Krystyn Smith <krskrsmith@gmail.com>
2022-05-02 15:18:00 -0500

    laying out annotations for new User class

---
+++ src/main/java/com/technews/model/User.java
@@ -0,0 +1,21 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table (name = "user")
+
+public class User {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
+}
